3652ed33031137bbc8cde9d6b6d8f0121128eb3c
Alex <alex.banar10@gmail.com>
2019-01-27 11:55:14 +0200

    The expanded base of the assignment. Showing of the movies list with RecyclerView  after parsing the data from movies json file. TODO: 1)DetailsActivity and pass movie item details to it after using parcelable plugin code generation for MovieItem data class. 2)Use SQLite to save movies list. 3)Retrieve from SQLite movies list and present on the screen it by the order of release year and from new to old

---
+++ app/src/main/java/android/alex/showofftaskex/MainActivity.java
@@ -0,0 +1,61 @@
+package android.alex.showofftaskex;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MovieDataSource.OnDataArrivedListener {
+
+    // /1) find the  Recycler
+    private RecyclerView rvMovies;
+    private LinearLayoutManager linearLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //1) find the  Recycler
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+
+        //2) Take data from the file and put it into List of Movie Items
+        MovieDataSource.getMovies(this, this);
+    }
+
+    @Override
+    public void onDataArrived(final ArrayList<MovieItem> movies, final Exception e) {
+        //this.runOnUiThread(new Runnable()
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (e == null) {
+                    //3) layout manager:
+                    linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+
+                    //4) init the adapter:
+                    MovieAdapter adapter = new MovieAdapter(/*getLayoutInflator(), */
+                            MainActivity.this, movies);
+
+                    //5) give the layout manager to the recycler.
+                    rvMovies.setLayoutManager(linearLayoutManager);
+
+                    //6) give the adapter to the recycler.
+                    rvMovies.setAdapter(adapter);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
